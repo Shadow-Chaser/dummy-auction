9e7560d028c2e77bd4329af78307083fc7fb4e95
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-03-01 22:21:15 +0600

    bug fixed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,11 +10,11 @@ const Cart = (props) => {
             <h3>My Team</h3>
             <h5>Player Added : {addedPlayer.length} </h5>
             <h5><FontAwesomeIcon icon={faUsers} /> Player List:</h5>
-            <ul>
+            <ol>
                 {
                     addedPlayer.map(player => <li><b>{player.name}</b> <br/> <small>Auction Money : $ {player.salary}</small> </li>)
                 }
-            </ul>
+            </ol>
             <h5><FontAwesomeIcon icon={faCoins} /> Total Budget : $ {totalBudget}</h5>
             
         </div>
