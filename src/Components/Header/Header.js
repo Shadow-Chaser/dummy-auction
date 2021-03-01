d500aa360c96a1914cf0f0d6aa723cb65a9b8d59
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-03-01 17:22:10 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="d-flex bg-info">
+            <h1 className="p-3">BPL Auction</h1>
+        </div>
+    );
+};
+
+export default Header;
